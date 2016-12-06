3719f60c5e85a78cdf4042193d9e29e245577545
Alberto Dal Dosso <daldosso@gmail.com>
2016-12-06 14:57:00 +0100

    Added a kind of login code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var app = express();
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+var express = require('express'),
+    app = express(),
+    mongodb = require("mongodb"),
+    ObjectID = mongodb.ObjectID,
+    USERS_COLLECTION = "users",
+    db;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -21,8 +23,27 @@ app.get("/spendykt", function(req, res) {
     });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.post("/spendykt-login", function(req, res) {
+    var newContact = req.body;
+    newContact.createDate = new Date();
+    db.collection(USERS_COLLECTION).insertOne(newContact, function(err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to create new contact.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
 });
 
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    db = database;
 
+    app.listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+    });
+
+});
